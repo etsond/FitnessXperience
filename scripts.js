5e2ea2b83f9acc72f65bb5140031c367c760a380
Etson <etsond@ufl.edu>
2023-09-29 20:33:53 -0400

    adding slider feature

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
-// lazy loading for faster page load
-document.addEventListener('DOMContentLoaded', function() {
-    const landing = document.querySelector('.landing');
-    landing.style.backgroundImage = `url('${landing.dataset.bg}')`;
-});
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const partnerLogos = document.querySelector('.partner-logos');
+    const partnerItems = partnerLogos.children;
+
+    let currentIndex = 0;
+    const totalPartners = partnerItems.length;
+    const partnerWidth = partnerItems[0].offsetWidth;
+
+    function showNextPartner() {
+      currentIndex = (currentIndex + 1) % totalPartners;
+      const offset = -partnerWidth * currentIndex;
+      partnerLogos.style.transform = `translateX(${offset}px)`;
+    }
+
+    setInterval(showNextPartner, 3000); // Automatically switch partners every 3 seconds
+  });
+
