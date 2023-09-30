982e69267991636ab82e16af0233e206f35c9801
Etson <etsond@ufl.edu>
2023-09-30 15:08:33 -0400

    adding more features, hamburger menu, more responsiveness styles

--- scripts.js
+++ scripts.js
@@ -14,5 +14,45 @@
     }
 
     setInterval(showNextPartner, 3000); // Automatically switch partners every 3 seconds
+
+
+    // transition item
+     const elements = document.querySelectorAll('.transition-item');
+
+    function isInViewport(el) {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function checkElements() {
+        elements.forEach(element => {
+            if (isInViewport(element)) {
+                element.classList.add('in-view');
+            }
+        });
+    }
+
+    window.addEventListener('scroll', checkElements);
+    window.addEventListener('resize', checkElements);
+
+    checkElements(); // Initial check
+
+
+    // hambureger menu
+    document.addEventListener('DOMContentLoaded', function() {
+  const mobileMenuToggle = document.querySelector('.mobile-menu-toggle');
+  const navigation = document.querySelector('.navigation');
+
+  mobileMenuToggle.addEventListener('click', function() {
+    navigation.classList.toggle('open');
+  });
+});
+
+// end of hamburger menu
   });
 
